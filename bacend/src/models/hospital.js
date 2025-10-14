2d9a0dc0306490a86a6731bed8be82795ced99e0
Le Quang Duy <quang125duy141@gmail.com>
2025-10-14 08:15:00 +0700

    add attribute image_hospital for model hospital

--- bacend/src/models/hospital.js
+++ bacend/src/models/hospital.js
@@ -1,14 +1,16 @@
+
 module.exports = (sequelize, DataTypes) => {
-const Hospital = sequelize.define("Hospital", {
-name: { type: DataTypes.STRING, allowNull: false },
-address: { type: DataTypes.STRING, allowNull: false },
-phone: DataTypes.STRING,
-email: DataTypes.STRING,
-description: DataTypes.TEXT,
-type: {
-type: DataTypes.ENUM("BENH_VIEN", "PHONG_KHAM_TU"),
-defaultValue: "BENH_VIEN"
-}
-});
-return Hospital;
+    const Hospital = sequelize.define("Hospital", {
+    name: { type: DataTypes.STRING, allowNull: false },
+    address: { type: DataTypes.STRING, allowNull: false },
+    phone: DataTypes.STRING,
+    email: DataTypes.STRING,
+    description: DataTypes.TEXT,
+    type: {
+        type: DataTypes.ENUM("BENH_VIEN", "PHONG_KHAM_TU"),
+        defaultValue: "BENH_VIEN"
+    },
+    image_hospital: DataTypes.STRING,
+    });
+    return Hospital;
 };
